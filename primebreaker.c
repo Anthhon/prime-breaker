7daeac06dbdaeb5b6584cf490f59b9451bb717a4
Anthhon <anthcomercial@gmail.com>
2023-01-01 20:18:22 -0300

    improved comments

--- primebreaker.c
+++ primebreaker.c
@@ -8,6 +8,8 @@
 
 void exit(int status);
 
+// This function checks if a given number is prime.
+// It returns TRUE if the number is prime, and FALSE if it is not.
 long unsigned is_number_prime(long unsigned number){
 
     // basic cases
@@ -19,19 +21,20 @@ long unsigned is_number_prime(long unsigned number){
     for (long unsigned i = 3; i <= sqrt(number); i += 2){
         if (number % i == 0) return FALSE;
     }
+    // if none of the conditions are met, the number is prime
     return TRUE;
 }
 
 
 void main(int argc, char* argv[]){
 
-    // check if there are enough arguments
+    // check if there is at least one command line argument
     if ( argc < 2 ){
         puts("error: missing target prime number argument");
         exit(0);
     }
 
-    long unsigned target_prime = strtoul(argv[1], NULL, 10); // put here the number to be found
+    long unsigned target_prime = strtoul(argv[1], NULL, 10);
     long unsigned fir_prime = 2, sec_prime = 2; // 0 and 1 are not valid numbers
 
     // generate numbers infinitely
@@ -39,22 +42,16 @@ void main(int argc, char* argv[]){
         if ( is_number_prime( fir_prime ) )
             sec_prime = 2; // restart variable
 
-            // just for testing
-            // printf("__TESTING %ld __\n", fir_prime);
-
             // test all combinations for multiplying values
-            // if the two prime number multiplied are the
-            // target number, return values and break
+            // if the two prime number multiplied are the target number,
+            // return the values and break
             while ( sec_prime <= fir_prime ){
                 if ( is_number_prime( sec_prime ) ){
                     if ( ( fir_prime * sec_prime ) == target_prime ){
                         puts("__COMBINATION FOUND__");
                         printf("first_number:%ld\nsecond_number:%ld\ntarget_number:%ld\n", fir_prime, sec_prime, target_prime);
                         exit(0); // stop code
-                    } else {
-                        // just for testing
-                        // printf("console: %ld did wrong, going to next\n", sec_prime);
-                    } 
+                    }
                 }
                 ++sec_prime; // update value for next loop iteration
             }   
