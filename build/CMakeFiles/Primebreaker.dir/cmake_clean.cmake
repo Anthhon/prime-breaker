9116790035579f119b2d0237e73705d963a2e111
Anthony Silva <anthcomercial@gmail.com>
2023-01-29 10:38:11 -0300

    using cmake to compile

---
+++ build/CMakeFiles/Primebreaker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Primebreaker.dir/primebreaker.c.o"
+  "CMakeFiles/Primebreaker.dir/primebreaker.c.o.d"
+  "Primebreaker"
+  "Primebreaker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Primebreaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
